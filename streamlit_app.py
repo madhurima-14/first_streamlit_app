a876cfdcbc680cd3bbe793fa80e93c2733436e10
madhurima-14 <121345727+madhurima-14@users.noreply.github.com>
2023-03-01 17:35:23 -0600

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Kale Salad & Smoothie')
+streamlit.header('🥗Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑🍞Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
